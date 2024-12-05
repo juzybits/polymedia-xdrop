f7b9ff1c4064e684c12247257f48bcef3ce0fed9
juzybits <96890594+juzybits@users.noreply.github.com>
2024-12-05 13:35:02 -0100

    js: link coinmeta

--- src/dev/link-local.sh
+++ src/dev/link-local.sh
@@ -6,9 +6,13 @@ set -o pipefail     # Prevent errors in a pipeline from being masked
 set -o xtrace       # Print each command to the terminal before execution
 
 PATH_SUITCASE=$HOME/data/code/polymedia-suitcase
+PATH_COINMETA=$HOME/data/code/polymedia-coinmeta
 PATH_PROJECT=$HOME/data/code/polymedia-xdrop
 
 cd $PATH_SUITCASE
+pnpm build
+
+cd $PATH_COINMETA
 pnpm build
 
 cd $PATH_PROJECT/src/sdk
@@ -17,3 +21,4 @@ pnpm link $PATH_SUITCASE/src/core
 cd $PATH_PROJECT/src/web
 pnpm link $PATH_SUITCASE/src/core
 pnpm link $PATH_SUITCASE/src/react
+pnpm link $PATH_COINMETA/src/sdk
