abeb0169ea24efe2bfae3f8c88c0242549a1157b
juzybits <96890594+juzybits@users.noreply.github.com>
2024-12-21 15:14:42 -0100

    dev: add upgrade-deps.sh

--- src/dev/link-local.sh
+++ src/dev/link-local.sh
@@ -5,9 +5,9 @@ set -o errexit      # Exit immediately if any command returns a non-zero status
 set -o pipefail     # Prevent errors in a pipeline from being masked
 set -o xtrace       # Print each command to the terminal before execution
 
+PATH_THIS=$HOME/data/code/polymedia-xdrop
 PATH_SUITCASE=$HOME/data/code/polymedia-suitcase
 PATH_COINMETA=$HOME/data/code/polymedia-coinmeta
-PATH_PROJECT=$HOME/data/code/polymedia-xdrop
 
 cd $PATH_SUITCASE
 pnpm build
@@ -15,10 +15,10 @@ pnpm build
 cd $PATH_COINMETA
 pnpm build
 
-cd $PATH_PROJECT/src/sdk
+cd $PATH_THIS/src/sdk
 pnpm link $PATH_SUITCASE/src/core
 
-cd $PATH_PROJECT/src/web
+cd $PATH_THIS/src/web
 pnpm link $PATH_SUITCASE/src/core
 pnpm link $PATH_SUITCASE/src/react
 pnpm link $PATH_COINMETA/src/sdk
