511aeba8f276f658cd591f4f0b3e788a8f07cf23
juzybits <96890594+juzybits@users.noreply.github.com>
2025-01-22 11:35:51 -0100

    dev: update dev scripts

--- src/dev/link-local.sh
+++ src/dev/link-local.sh
@@ -5,9 +5,11 @@ set -o errexit      # Exit immediately if any command returns a non-zero status
 set -o pipefail     # Prevent errors in a pipeline from being masked
 set -o xtrace       # Print each command to the terminal before execution
 
-PATH_THIS=$HOME/data/code/polymedia-xdrop
-PATH_SUITCASE=$HOME/data/code/polymedia-suitcase
-PATH_COINMETA=$HOME/data/code/polymedia-coinmeta
+SCRIPT_DIR="$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )"
+PROJECT_ROOT="$( cd "$SCRIPT_DIR/../.." && pwd )"
+
+PATH_SUITCASE="$PROJECT_ROOT/../polymedia-suitcase"
+PATH_COINMETA="$PROJECT_ROOT/../polymedia-coinmeta"
 
 cd $PATH_SUITCASE
 pnpm build
@@ -15,10 +17,10 @@ pnpm build
 cd $PATH_COINMETA
 pnpm build
 
-cd $PATH_THIS/src/sdk
+cd $PROJECT_ROOT/src/sdk
 pnpm link $PATH_SUITCASE/src/core
 
-cd $PATH_THIS/src/web
+cd $PROJECT_ROOT/src/web
 pnpm link $PATH_SUITCASE/src/core
 pnpm link $PATH_SUITCASE/src/react
 pnpm link $PATH_COINMETA/src/sdk
--- src/dev/upgrade-deps.sh
+++ src/dev/upgrade-deps.sh
@@ -5,17 +5,19 @@ set -o errexit      # Exit immediately if any command returns a non-zero status
 set -o pipefail     # Prevent errors in a pipeline from being masked
 set -o xtrace       # Print each command to the terminal before execution
 
-PATH_THIS=$HOME/data/code/polymedia-xdrop
+SCRIPT_DIR="$( dirname "$(readlink -f "${BASH_SOURCE[0]}")" )"
+PROJECT_ROOT="$( cd "$SCRIPT_DIR/../.." && pwd )"
+
 REACT_VERSION="^18.0.0"
 
-cd $PATH_THIS
+cd $PROJECT_ROOT
 pnpm up --latest --recursive
 pnpm up --latest -w
 
-cd $PATH_THIS/src/web
+cd $PROJECT_ROOT/src/web
 pnpm add react@$REACT_VERSION react-dom@$REACT_VERSION
 pnpm add -D @types/react@$REACT_VERSION @types/react-dom@$REACT_VERSION
 
-cd $PATH_THIS
+cd $PROJECT_ROOT
 pnpm up --recursive
 pnpm up -w
